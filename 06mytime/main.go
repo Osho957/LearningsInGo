6e61ca7f50a2461fe845a33c2f8340b55ad11172
Osho957 <oshorajneesh453@gmail.com>
2024-01-28 00:34:22 +0530

    time basics and go build

--- 06mytime/main.go
+++ 06mytime/main.go
@@ -13,4 +13,17 @@ func main()  {
 
 	// using exact this format to get date
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2001,time.May,06,06,06,00,0,time.UTC)
+	fmt.Println("Created At: ",createdDate)
+	fmt.Println(createdDate.Format("01-02-2006"))
+
+	var local = time.Now().Nanosecond()
+	fmt.Println(local)
 }
+
+/*
+go build -> to build executable files
+GOOS = "windows" for window os
+       "linux" for linux
+*/
